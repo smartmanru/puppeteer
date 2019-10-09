dfe183a4ebd95a34cad03b7b3b57aff6e7e569ca
Your Name <you@example.com>
2019-10-09 22:05:56 +0300

    com

--- app.js
+++ app.js
@@ -1,46 +1,66 @@
 var _=require('underscore')
 var puppeteer = require('puppeteer');
+var cve_pr=0;
+var cvim_ve=[];
+var report=[];
 var  arr = require('./link');
 var aurl_cve=[]
 var vm = this;
-for (let link of arr){
-  setTimeout(function(){
+var linkb=[];
+setTimeout(function(){
 (async () => {
   var browser = await puppeteer.launch(
     {headless:true,args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    for (let linka of arr){
     var page = await browser.newPage();
-    //   await page.goto(arr[i], {waitUntil: 'networkidle2'});
-    
-    
     try {
       // Попробуем перейти по URL
-      await page.goto(link, {waitUntil: 'networkidle2'});
-      console.log('Открываю страницу:' + link);
-      var postsSelector = '#vulnslisttable > tbody > tr > td > a';
-      await page.waitForSelector(postsSelector, { timeout: 0 });
+      await page.goto(linka, {waitUntil: 'networkidle2'});
+      console.log('Открываю страницу:' + linka);
+      var postsSelector = '#vulnslisttable > tbody > tr > td:nth-child(2) > a';
+
+	await page.waitForSelector(postsSelector, { timeout: 0 });
       var postUrls = await page.$$eval(
         postsSelector, cve_links=>cve_links.map(link=>link.href));
-        var url_cve=_.compact(postUrls)
-        await browser.close()
-        
-        // console.log(postUrls);
+
+	    // console.log('Открываю страницу:' + linka);
+        var url_cve=_.compact(postUrls);
+        // await browser.close()
+	for (let linkb of url_cve){
+	    await page.goto(linkb, {waitUntil: 'networkidle2'});
+      console.log('Открываю страницу:' + linkb);
+	var vers='#vulnprodstable > tbody > tr> td:nth-child(5)'
+
+	var prod='#vulnprodstable > tbody > tr> td:nth-child(4)'
+//        console.log(url_cve);
+	await page.waitForSelector(prod, { timeout: 0 });
+    	var cvim_vers = await page.$$eval(
+        vers, cvim_ve=>cvim_ve.map(link=>link.textContent));
+
+    	var cve_prod = await page.$$eval(
+        prod, cve_pr=>cve_pr.map(link=>link.textContent));
+        // console.log('Открываю страницу:' + linkb);
+        cve_pr=_.uniq(cve_pr).toString
+  var post=[cve_pr,[cvim_ve]]
+		var report=_.compact(post)
+	report.push(post)
+        // await browser.close()
+	}
     } catch (error) {
-      console.log(`Не удалось открытm страницу: ${arr[i]} из-за ошибки: ${error}`);
-    
+      console.log(`Не удалось открытm страницу: ${linka} ${linkb} из-за ошибки: ${error}`);
       await browser.close()
+
     }
 // Найдём все ссылки на статьи   
 
 
-console.log(url_cve);
+// console.log(url_cve);
 aurl_cve.push({link:[url_cve]})
 
-}
+}}
 )
 ();
 }, 1000);
-
-}
 
 
 
